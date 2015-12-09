eb6ce11982c3af08dd920709bbe4733a0e8fc3eb
Xavier Mamano <xavier.mamano@gmail.com>
2015-12-09 18:29:11 +0100

    order_report_prod_fam: Poder treure el 2% descompte i no mostrar si 0

--- local_custom/order_report_prod_fam.php
+++ local_custom/order_report_prod_fam.php
@@ -35,35 +35,51 @@ function write_orders_prod($date_for_order, $provider_id, $order_id, $detail) {
     $strSQL .= 'pv_name, order_id';
     $rs = $db->Execute($strSQL);
     while ($row = $rs->fetch_array()) {
+        $ppcDescompte = 0;
         $row_pv = get_row_query(
             "SELECT notes FROM aixada_provider where id={$row['provider_id']}");
-        if (!is_null($row_pv['notes']) && $row_pv['notes'] === "FAM" && $detail) {
-            write_order_prod_fam(
+        if (is_null($row_pv['notes'])) {
+            $tipus_ordre = 'PRO';            
+        } else {
+            $tipus_ordre = substr($row_pv['notes'],0,3);
+            if (is_numeric(substr($row_pv['notes'],3))) {
+                $ppcDescompte = substr($row_pv['notes'],3) + 0;
+            } 
+        }
+        if ($tipus_ordre === "FAM" &&
+                $detail) {
+            $html_f = write_order_prod_fam(
                 $row['date_for_order'], 
                 $row['provider_id'], 
                 $row['order_id'],
+                $ppcDescompte,
                 false, true, false
             );
             write_order_fam_prod(
                 $row['date_for_order'], 
                 $row['provider_id'], 
                 $row['order_id'],
-                $detail, false, true
-            );			
+                $ppcDescompte,
+                $detail, false, false
+            );
+            echo $html_f;
         } else {
             write_order_prod_fam(
                 $row['date_for_order'], 
                 $row['provider_id'], 
                 $row['order_id'],
+                $ppcDescompte,
                 $detail
             );
         }
     }
 }
-function write_order_prod_fam($date_for_order, $provider_id, $order_id, $detail,
+function write_order_prod_fam($date_for_order, $provider_id, $order_id, 
+        $ppcDescompte, $detail,
         $head = true, $foot = true) {
     $db = DBWrap::get_instance();
     
+    $coefQu = 100.0/(100.0 - $ppcDescompte);
     $strSQL = get_SQL($date_for_order, $provider_id, $order_id);
     $cfg = configuration_vars::get_instance();
     if (isset($cfg->order_review_uf_sequence) &&
@@ -83,13 +99,15 @@ function write_order_prod_fam($date_for_order, $provider_id, $order_id, $detail,
     $total_cost = 0;
     $total_amount = 0;
     $aaa_quantity =0;
+    $pro_count = 0;
     $html = '';
     while ($row = $rs->fetch_array()) {
+        if ($row['quantity'] == 0) { continue; }
         if ($brk['1_provider_id'] != $row['provider_id'] || 
                     $brk['1_order_id'] != $row['order_id']) {
             // end previus order
             $html .= break2Html_end($brk, $detail);
-            $html .= orderHtml_end($total_cost, $total_amount, 
+            $html .= orderHtml_end($total_cost, $total_amount,  $ppcDescompte,
                 $brk['1_desc_order']);
             // start order
             $total_cost = 0;
@@ -109,12 +127,18 @@ function write_order_prod_fam($date_for_order, $provider_id, $order_id, $detail,
                 'oi.product_id = '.$brk['2_id_break']);
             $sum_quantity = $sum_quantity_row['sum_quantity'];
             // Rounded by product
-            $total_cost += round($row['cost_price']*$sum_quantity, 2);
+            $total_cost += round($row['cost_price']*$sum_quantity*$coefQu, 2);
             if ($detail) {
                 $html .= '<div class="block">';
             } else {
-                $html .= '<div class="block" style="margin-top: 0.25cm">';
+                $border_det = "border-bottom: dotted 1px #777;";
+                if ($pro_count === 0) {
+                    $border_det .= "border-top: dotted 1px #777;";
+                }
+                $html .= '<div class="block" style="'.$border_det.
+                    'margin-top: 0.25cm; ">';
             }
+            $pro_count++;
             $html .= '<h3><div class="cel13">'.$row['p_name'].
                     '<div class="lite" style="float:right">'.$row['um_name'].'</div>'.
                 '</div>'.
@@ -123,7 +147,7 @@ function write_order_prod_fam($date_for_order, $provider_id, $order_id, $detail,
                 'x<div class="cel1h aprov">'.
                     number_format($row['cost_price'], 2).'</div>'.
                 '=<div class="cel2 aprov">'.
-                    number_format($row['cost_price']*$sum_quantity, 2).'</div>'.
+                    number_format($row['cost_price']*$sum_quantity*$coefQu, 2).'</div>'.
                 '</h3>'.chr(10);
             if ($detail) {
                 $html .= '<div class="block-body">'.chr(10);
@@ -141,17 +165,20 @@ function write_order_prod_fam($date_for_order, $provider_id, $order_id, $detail,
         }
     }
     $html .= break2Html_end($brk, $detail);
+    $html_f = orderHtml_end($total_cost, $total_amount, $ppcDescompte,
+        $brk['1_desc_order']);
     if ($foot) {
-        $html .= orderHtml_end($total_cost, $total_amount, 
-            $brk['1_desc_order']);
+        $html .= $html_f;
     }
     echo $html;
+    return $html_f;
 }
 function write_order_fam_prod(
-        $date_for_order, $provider_id, $order_id, $detail,
+        $date_for_order, $provider_id, $order_id, $ppcDescompte, $detail,
         $head = true, $foot = true) {
     $db = DBWrap::get_instance();
     
+    $coefQu = 100.0/(100.0 - $ppcDescompte);
     $strSQL = get_SQL($date_for_order, $provider_id, $order_id);
     $cfg = configuration_vars::get_instance();
     if (isset($cfg->order_review_uf_sequence) &&
@@ -173,12 +200,13 @@ function write_order_fam_prod(
     $aaa_quantity =0;
     $sum_quantity=0;
     $html = '';
-    while ($row = $rs->fetch_array()) {
+    while ($row = $rs->fetch_array()) {        
+        if ($row['quantity'] == 0) { continue; }
         if ($brk['1_provider_id'] != $row['provider_id'] || 
                     $brk['1_order_id'] != $row['order_id']) {
             // end previus order
             $html .= break2Html_end($brk, $detail);
-            $html .= orderHtml_end($total_cost, $total_amount, 
+            $html .= orderHtml_end($total_cost, $total_amount, $ppcDescompte,
                 $brk['1_desc_order']);
             // start order
             $total_cost = 0;
@@ -203,30 +231,31 @@ function write_order_fam_prod(
         }
         // Rounded by UF
         $total_amount += round($row['final_price']*$row['quantity'], 2);
-        $total_cost += round($row['cost_price']*$row['quantity'], 2);
+        $total_cost += round($row['cost_price']*$row['quantity']*$coefQu, 2);
         if ($detail) {
             $border_det = "border-bottom: dotted 1px #777;";
             if ($det_lin === 0) {
                 $border_det .= "border-top: dotted 1px #777;";
             }
             $det_lin++;
-            $html .= '<div style="'.$border_det.
-                    ' margin-left:.5cm; padding:.05cm 0">';
+            $html .= '<div style="'.$border_det.' padding:.05cm 0">';
             $html .= 
                 '<div class="cel1h num">'.
                         clean_zeros($row['quantity']).'</div>'.
-                '<div class="cel1">x&nbsp;</div>'.
-                '<div class="cel8">'.$row['p_name'].'</div>'.
-                '<div class="cel4 lite">('.$row['um_name'].')</div>';
+                '<div class="cel0h center">x</div>'.
+                '<div class="cel12">'.$row['p_name'].'</div>'.
+                '<div class="cel3 lite">('.$row['um_name'].')</div>';
             $html .= '</div>'.chr(10);
         }
     }
     $html .= break2Html_end($brk, $detail);
-    if ($foot || true) {
-        $html .= orderHtml_end($total_cost, $total_amount,
+    $html_f = orderHtml_end($total_cost, $total_amount, $ppcDescompte,
             $brk['1_desc_order']);
+    if ($foot) {
+        $html .= $html_f;
     }
     echo $html;
+    return $html_f;
 }
 function orderHtml_start(&$brk, $row) {
     $brk['1_order_id'] = $row['order_id'];
@@ -238,7 +267,7 @@ function orderHtml_start(&$brk, $row) {
         '['.formatOrderStatus($row['revision_status']).']'.
         '</h2>';
 }
-function orderHtml_end($total_cost, $total_amount, $desc) {
+function orderHtml_end($total_cost, $total_amount, $ppcDescompte, $desc) {
     if ($desc == null) {
         return '';
     }
@@ -248,7 +277,9 @@ function orderHtml_end($total_cost, $total_amount, $desc) {
                 '<span class="cel2 num gray">'.
                     number_format($total_amount,2).
                 '</span>'.
-                '<div class="cel3 num lite gray">Cost sense IVA:</div>'.
+                '<div class="cel3 num lite gray">Cost sense IVA:'.
+                    ($ppcDescompte ? "<br>(falta {$ppcDescompte}%)" : '').
+                    '</div>'.
                 '<div class="cel2 aprov">'.
                     number_format($total_cost,2).
                 '</div>'.
